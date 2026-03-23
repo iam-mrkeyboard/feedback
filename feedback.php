d0afa8f3f91ed1c3482e63f781ffdb6abd527170
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2026-03-23 17:43:29 +0300

    Refactor feedback display logic to utilize dynamic data and improve structure

--- feedback.php
+++ feedback.php
@@ -1,22 +1,57 @@
 <?php include 'inc/header.php'?>
+
+<?php 
+
+$feedback =[
+
+
+  [
+    'id' => 1,
+    'email' => 'john@example.com',
+    'name' => 'John Doe',
+    'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!',
+    'date' => '2024-01-15'
+  ],
+  [
+    'id' => 2,
+    'email' => 'jane@example.com',
+    'name' => 'Jane Smith',
+    'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!',
+    'date' => '2024-01-16'
+  ],
+  [
+    'id' => 3,
+    'email' => 'bob@example.com',
+    'name' => 'Bob Johnson',
+    'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!',
+    'date' => '2024-01-17'
+  ]
+];
+
+?>
+
     <h2>Feedback</h2>
 
-    <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-     </div>
-   </div>
+    <?php  if(empty($feedback)): ?>
 
-   <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-     </div>
-   </div>
+      <p class="lead mt3">There is no feedback</p>
 
-   <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+
+      <?php endif; ?>
+
+      <?php foreach($feedback as $item): ?>
+
+    <div class="card my-3  w-75">
+     <div class="card-body text-center">
+     <?php echo $item['body'] ?>
+     <div class="text-secondary mt2">
+
+     by <?php echo $item['name'] ?>
+     </div>
      </div>
    </div>
+<?php endforeach ?>
+
+  
  <?php include 'inc/footer.php' ?>
 
