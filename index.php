4f9415ae3a4d80e8e4caf97945d685ce06a266e1
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2026-03-24 15:19:24 +0300

    Implement feedback form validation and submission logic with error handling

--- index.php
+++ index.php
@@ -1,24 +1,92 @@
-<?php include 'inc/header.php'?>
-    <img src="/my-first-php-code/feedback/img/logo.png" class="w-25 mb-3" alt="">
-    <h2>Feedback</h2>
-    <p class="lead text-center">Leave feedback for Traversy Media</p>
-    <form action="" class="mt-4 w-75">
-      <div class="mb-3">
-        <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
-      </div>
-      <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
-      </div>
-      <div class="mb-3">
-        <label for="body" class="form-label">Feedback</label>
-        <textarea class="form-control" id="body" name="body" placeholder="Enter your feedback"></textarea>
-      </div>
-      <div class="mb-3">
-        <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
-      </div>
-    </form>
-  
+<?php include 'inc/header.php' ?>
 
-    <?php  include 'inc/footer.php' ?>
+<?php
+
+$name = $email = $body = '';
+
+$nameError = $emailError = $bodyError = '';
+
+// Submit
+
+if (isset($_POST['submit'])) {
+
+  // Validate name
+
+  if (empty($_POST['name'])) {
+    $nameError = 'Name is required';
+  } else {
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  }
+  // Validate email
+
+  if (empty($_POST['email'])) {
+    $emailError = 'Email is required';
+  } else {
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+  }
+  // Validate body
+
+  if (empty($_POST['body'])) {
+    $bodyError = 'Feedback is required';
+  } else {
+    $body = filter_input(INPUT_POST, 'body', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  }
+
+
+  if (empty($nameError) && empty($emailError) && empty($bodyError)) {
+
+    // Add to the database
+
+    $sql = "INSERT INTO feedback (name, email, body) VALUES ('$name', '$email', '$body')";
+
+    if (mysqli_query($conn, $sql)) {
+
+      // Success
+      header('Location: feedback.php');
+    } else {
+      echo 'Error' . mysqli_errno($conn);
+    }
+  }
+}
+
+
+
+
+?>
+
+
+
+<img src="/my-first-php-code/feedback/img/logo.png" class="w-25 mb-3" alt="">
+<h2>Feedback</h2>
+<p class="lead text-center">Leave feedback for Traversy Media</p>
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" class="mt-4 w-75" method="POST">
+  <div class="mb-3">
+    <label for="name" class="form-label">Name</label>
+    <input type="text" class="form-control <?php echo !$nameError ?:  'is-invalid' ?> " id="name" name="name" placeholder="Enter your name">
+
+    <div class="invalid-feedback">
+      <?php echo $nameError ?>
+    </div>
+  </div>
+  <div class="mb-3">
+    <label for="email" class="form-label">Email</label>
+    <input type="email" class="form-control <?php echo !$emailError ?:  'is-invalid' ?> " id="email" name="email" placeholder="Enter your email">
+
+    <div class="invalid-feedback">
+      <?php echo $emailError ?>
+    </div>
+  </div>
+  <div class="mb-3">
+    <label for="body" class="form-label">Feedback</label>
+    <textarea class="form-control <?php echo !$bodyError ?:  'is-invalid' ?> " id="body" name="body" placeholder="Enter your feedback"></textarea>
+    <div class="invalid-feedback">
+      <?php echo $bodyError ?>
+    </div>
+  </div>
+  <div class="mb-3">
+    <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
+  </div>
+</form>
+
+
+<?php include 'inc/footer.php' ?>
